a03534df111e7514368d318b4d4273f8b5c4e443
MinHo <my_alsgh@naver.com>
2022-05-04 04:01:15 +0900

    Update app/src/main/java/com/example/soptexampleproject/presentation/home/screens/adapter/ViewPagerAdapter.kt
    Co-authored-by: choiyoungjin <cyjin6789@gmail.com>

--- app/src/main/java/com/example/soptexampleproject/presentation/home/screens/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/soptexampleproject/presentation/home/screens/adapter/ViewPagerAdapter.kt
@@ -7,9 +7,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
     val fragments = mutableListOf<Fragment>()
-    override fun getItemCount(): Int {
-        return fragments.size
-    }
+    override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment = fragments[position]
 }
